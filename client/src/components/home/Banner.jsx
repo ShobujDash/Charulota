0d1f374f1d124505e731de3fee27449d4251dafe
ShobujDas <shobujd6.gmail.com>
2024-11-05 02:19:31 +0600

    Category added

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -4,6 +4,9 @@ import { assets } from '../../assets/assets';
 const Banner = () => {
   return (
     <>
+      <div>
+        
+      </div>
       <div className="w-full h-40 sm:h-56 md:h-96 rounded-md relative">
         <video className='w-full h-full object-cover rounded-md' src={assets.bannerVedio} autoPlay loop muted />
         <div className='absolute w-[100%] h-[100%] top-0 flex flex-col items-center justify-end'>
